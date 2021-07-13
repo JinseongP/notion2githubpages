c3591b63b2ffdce7b5e32eff41579e59cfcb28f2
Jinseong Park <wlstjddl97@naver.com>
2021-07-13 23:21:12 +0900

    v0.1

--- notion2githubpages.py
+++ notion2githubpages.py
@@ -11,7 +11,7 @@ import shutil
 import os
 
 
-# In[ ]:
+# In[2]:
 
 
 target="Export-*.zip"
@@ -29,20 +29,20 @@ if not os.path.exists(images_folder_path):
     os.makedirs(images_folder_path)
 
 
-# In[ ]:
+# In[4]:
 
 
-for path in path_list:
-
+for path in path_list:    
+    
     zip_path = zipfile.ZipFile(path)
-
+    
     images = None
     if len(zip_path.namelist())>1:
         images = zip_path.namelist()[1:]
     post = zip_path.namelist()[0]
 
     exported_filename = post.split('.')[0]
-
+ 
     ########################### input
     print("Title for "+post+": ")
     title = input ()
@@ -53,7 +53,7 @@ for path in path_list:
     print("Tags (separated by '/') for "+post+": ")
     tags = input()
     tags = tags.split('/')
-
+    
     filename = str(date.tm_year)+"-"+str(date.tm_mon)+"-"+str(date.tm_mday)+"-"+title
     ###########################
 
@@ -61,9 +61,9 @@ for path in path_list:
     zip_path.extractall()
 
     with open(post, 'r+', encoding='UTF8') as file:
-        readcontent = file.read()
+        readcontent = file.read()  
         file.seek(0, 0)
-        file.truncate(0)
+        file.truncate(0) 
         file.write("---"+"\n")
         file.write("title: "+title+"\n")
         file.write("excerpt: "+excerpt+"\n")
@@ -72,21 +72,27 @@ for path in path_list:
         for tag in tags:
             file.write("  - "+tag+"\n")
         file.write("---"+"\n")
-
+              
         if images is not None:
             for line in readcontent.splitlines():
                 if 'png' in line:
                     imagename = (line.split('.')[0].split('/')[1])
-                    line = '!['+imagename+'.png](%./../../'+images_folder_path+filename+'/'+imagename+'.png)'
-                file.write(line+'\n')
+                    line = '!['+imagename+'.png]({{site.url}}/'+images_folder_path+filename+'/'+imagename+'.png){:.center-image}'
+                file.write(line+'\n')        
         else:
-            file.write(readcontent)
+            file.write(readcontent) 
 
         file.close()
 
     shutil.move(post, posts_folder_path+filename+".md")
     if images is not None:
         shutil.move(exported_filename, images_folder_path+filename)
+    zip_path.close()
+    os.remove(path)
 
 
 # In[ ]:
+
+
+
+
